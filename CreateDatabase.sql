f5304a9dcaf5525e49e0b422d85b2284db50dace
Arnab Roy Chowdhury <arnab-roy-chowdhury@outlook.com>
2013-06-30 00:49:14 +0530

    Update CreateDatabase.sql

--- CreateDatabase.sql
+++ CreateDatabase.sql
@@ -4,7 +4,7 @@ GO
 CREATE DATABASE Lession4Db ON PRIMARY
 (
 	NAME = N'Lession4Db',
-	FILENAME = N'C:\Users\Arnab\Documents\My Place\Technical\Tutorials and Resources\Sql Server\Sample Databases\AccessPrac\Lession4\Lession4Db.mdf',
+	FILENAME = N'C:\Lession4\Lession4Db.mdf',
 	SIZE = 3MB,
 	MAXSIZE = UNLIMITED,
 	FILEGROWTH = 10%
@@ -12,7 +12,7 @@ CREATE DATABASE Lession4Db ON PRIMARY
 LOG ON
 (
 	NAME = N'Lession4DbLog',
-	FILENAME = N'C:\Users\Arnab\Documents\My Place\Technical\Tutorials and Resources\Sql Server\Sample Databases\AccessPrac\Lession4\Lession4DbLog.ldf',
+	FILENAME = N'C:\Lession4\Lession4DbLog.ldf',
 	SIZE = 2MB,
 	MAXSIZE = UNLIMITED,
 	FILEGROWTH = 10%
